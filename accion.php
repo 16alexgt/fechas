a7fd29a99f7f4883355606919f4b697a69a445b0
Alex Gutierrez <danielgutita2018@gmail.com>
2025-05-28 13:37:25 -0500

    Add files via upload

---
+++ accion.php
@@ -0,0 +1,70 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener la fecha ingresada
+    $fechaIngresada = $_POST['fecha'];
+
+    // Convertir la fecha a un objeto DateTime
+    $fecha = DateTime::createFromFormat('Y-m-d', $fechaIngresada);
+
+    if ($fecha) {
+        // Calcular las fechas
+        $fecha100Dias = clone $fecha;
+        $fecha100Dias->modify('+100 days');
+
+        $fecha80Dias = clone $fecha;
+        $fecha80Dias->modify('+85 days');
+
+        $fechaParto = clone $fecha;
+        $fechaParto->modify('+114 days');
+
+        // Función para traducir días y meses a español
+        function formatearFechaEnEspañol($fecha) {
+            $dias = [
+                'Sunday' => 'domingo',
+                'Monday' => 'lunes',
+                'Tuesday' => 'martes',
+                'Wednesday' => 'miércoles',
+                'Thursday' => 'jueves',
+                'Friday' => 'viernes',
+                'Saturday' => 'sábado'
+            ];
+
+            $meses = [
+                'January' => 'enero',
+                'February' => 'febrero',
+                'March' => 'marzo',
+                'April' => 'abril',
+                'May' => 'mayo',
+                'June' => 'junio',
+                'July' => 'julio',
+                'August' => 'agosto',
+                'September' => 'septiembre',
+                'October' => 'octubre',
+                'November' => 'noviembre',
+                'December' => 'diciembre'
+            ];
+
+            $diaSemana = $dias[$fecha->format('l')];
+            $diaMes = $fecha->format('d');
+            $mes = $meses[$fecha->format('F')];
+            $año = $fecha->format('Y');
+
+            return "$diaSemana, $diaMes de $mes de $año";
+        }
+
+        // Mostrar los resultados en el modal
+        echo "<script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    var resultList = document.getElementById('resultList');
+                    resultList.innerHTML += '<li class=\"list-group-item result-item\">Fecha Purga 100 días: " . formatearFechaEnEspañol($fecha100Dias) . "</li>';
+                    resultList.innerHTML += '<li class=\"list-group-item result-item\">Fecha Coli: " . formatearFechaEnEspañol($fecha80Dias) . "</li>';
+                    resultList.innerHTML += '<li class=\"list-group-item result-item\">Fecha de parto: " . formatearFechaEnEspañol($fechaParto) . "</li>';
+                    $('#resultModalLabel').text('Resultados - " . formatearFechaEnEspañol($fecha) . "');
+                    $('#resultModal').modal('show');
+                });
+              </script>";
+    } else {
+        echo "<div class='alert alert-danger mt-4'>La fecha ingresada no es válida.</div>";
+    }
+}
+?>
